b1ce010c299267de830bd01f50a2f68a4a77dbf5
akash07marwah <akashmarwah07@gmail.com>
2019-08-15 21:41:17 +0530

    Adds Frontend

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const connectDB =  require('./config/db')
+const app = express()
+const cors = require('cors')
+connectDB()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use('/',require('./routes/index'))
+app.use('/api/url',require('./routes/url'))
+
+const PORT = process.env.PORT || 3001; 
+
+app.listen(PORT,()=>{
+    console.log(`server running on https://localhost:${PORT}`)
+})
